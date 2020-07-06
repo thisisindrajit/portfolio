7d40c4e2e4647edb5fe18637b1f22474bda6d1fb
Indrajit <indrajitvijayakumar@gmail.com>
2020-07-06 18:40:21 +0530

    added skeleton for favourites area

--- src/Favourites.jsx
+++ src/Favourites.jsx
@@ -3,7 +3,22 @@ import React from 'react';
 const Favourites = () =>{
     return(
     <div id="fav">
-        This is the fav section!
+        <div className="favsection">
+            This is fav section for Songs
+        </div>
+
+        <div className="favsection colorchange">
+            This is fav section for Books
+        </div>
+
+        <div className="favsection colorchange">
+            This is fav section for Movies
+        </div>
+
+        <div className="favsection">
+            This is inspiration section
+        </div>
+
     </div>);
 }
 
