d83a62c51dcc48bc47eed6534dfb206a0b388fb2
Indrajit <indrajitvijayakumar@gmail.com>
2020-07-09 20:13:59 +0530

    added few cool tooltips to social cards

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -10,10 +10,10 @@ const MainComponent = () => {
                     India! </span><div className="tag">{"</span>"}</div></h1>
 
                 <div id="socialcards">
-                <SocialCard src="./linkedin.png" url="https://www.linkedin.com/in/indrajit-vijayakumar-6b761869"/>
-                <SocialCard src="./github.png" url="https://github.com/thisisindrajit"/>
-                <SocialCard src="./twitter.png" url="https://twitter.com/indrajitmusic"/>
-                <SocialCard src="./youtube.png" url="https://www.youtube.com/channel/UCvd_7o_qpO2VC6HPn7Huh_w?view_as=subscriber"/>
+                <SocialCard src="./linkedin.png" url="https://www.linkedin.com/in/indrajit-vijayakumar-6b761869" data="LinkedIn"/>
+                <SocialCard src="./github.png" url="https://github.com/thisisindrajit" data="GitHub"/>
+                <SocialCard src="./twitter.png" url="https://twitter.com/indrajitmusic" data="Twitter"/>
+                <SocialCard src="./youtube.png" url="https://www.youtube.com/channel/UCvd_7o_qpO2VC6HPn7Huh_w?view_as=subscriber" data="YouTube"/>
                 </div>
 
             </div>
